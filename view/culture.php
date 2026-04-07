bdcd4a6187333811a93c85d7318a7fdd8a2eb6a6
John-Dav9 <tchomguijohn@gmail.com>
2026-04-07 14:53:27 +0200

    Ajout de la page culture.php et son contenu, modification du contenu de la page histoire.php et error404.php

---
+++ view/culture.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TI1 - SITE DE BRUXELLE</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div>
+        <img src="../img/banniere.jpg" alt="">
+        <h1>CAPENHAGUE</h1>
+    </div>
+
+    <header>
+        <?php
+        // chemin absolu
+        include ROOT_PATH."/view/inc/menu.php";
+        // chemin relatif (en PHP à partir de la page qui appelle)
+        # include "inc/menu.php";
+
+        ?>
+    </header>
+
+    <main>
+        <section style="text-align: center; margin-top: 10vh;">
+            <h1>Histoire</span></h1>
+            <img src="../img/histoire.png" alt="Histoire de Capenhagen">
+
+            <h3>Bibliothèques</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Le Diamant noir (1999) qui abrite la bibliothèque nationale du Danemark conçue par les architectes Schmidt Hammer Lassen.
+                Il existe à Copehague 20 bibliothèques dont une principale qui est la plus grande du pays. Celle-ci est située dans le quartier latin de la ville.
+
+            </p>
+            <h3>Chateaux</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Palais Amalienborg
+                Christiansborg
+                Château de Frederiksborg à Hillerød
+                Château Kronborg à Helsingør
+                Château de Rosenborg
+                Rundetårn
+                Kastellet
+            </p>
+            <h3>Musées</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Statens Museum for Kunst (Musée des Beaux-arts danois)
+                Musée d'art moderne Louisiana à Humlebæk
+                Musée national du Danemark
+                Ny Carlsberg Glyptotek
+                La collection Hirschsprung
+                Musée de la liberté (Frihedsmuseet)
+                Museum Erotica (Musée de l'érotisme) (fermé)
+                Eksperimentarium (musée de physique expérimentale)
+                Planétarium Tycho Brahé
+            </p>
+
+            <h3>Jardins</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Bakken : l'un des plus anciens parcs d'attractions du monde
+                Parc des daims: en dehors de Copenhague, au sein duquel se trouve Bakken
+                Copenhagen Zoo
+                Kongens Have : jardin royal
+                Jardins de Tivoli : parc d'attractions
+                Fælledparken : le plus grand parc urbain de Copenhague
+                Cimetière Assistens Kirkegaard : un cimetière datant de 1760
+            </p>
+
+            <h3>Personnages connus, associés à Capenhague</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Hans Christian Andersen (1805-1875), écrivain
+                Mianne Bagger (1966-), golfeuse
+                Anthon Bang (1809-1870), écrivain et éditeur norvégien
+                Niklas Björnberg (1758-1829), homme d'affaires et directeur de la Compagnie suédoise des Indes orientales
+                Niels Bohr (1885-1962), physicien
+                Jens Bodtker Rasmussen (1947-2005), herpétologiste
+                Tycho Brahe (1546-1601), astronome
+                Aura Dione (née en 1985), chanteuse.
+                Nanna Ditzel (1923-2005), architecte et designer
+                Helen Dohlmann (1870-1942), sculptrice
+                Carl Theodor Dreyer (1889-1968), cinéaste
+                Giancarlo Esposito (né en 1958), acteur
+                Emma Gad (1852-1921), écrivaine
+                Niels Wilhelm Gade (1817-1890), compositeur
+                Jan Gehl (né en 1936), urbaniste et architecte
+                Rolf Graae (1916-1996), architecte
+                Nikolai Grundtvig (1783-1872), philosophe, écrivain et poète
+                August Wilhelm Hartmann (1775-1850), compositeur danois
+                Johann Peter Emilius Hartmann (1805-1900), compositeur danois
+                Ludvig Holberg (1684-1754), historien et dramaturge
+                Bodil Ipsen (1889-1964), réalisatrice
+                Victor Isbrand (1897-1989), peintre
+                Arne Jacobsen (1902-1971), architecte
+                Robert Jacobsen (1912-1993), artiste
+                Grete Jalk (1920–2006), designer
+                Niels Peter Jensen (1802 -1846), compositeur, flûtiste et organiste
+                Søren Kierkegaard (1813-1855), philosophe
+                Per Kirkeby (né en 1938) artiste
+                Mads Mikkelsen (né en 1965), acteur
+                Georg Mohr (1640-1697), mathématicien
+                Carl Nielsen (1865-1931), compositeur
+                Aaron Nimzowitsch (1886-1935), théoricien du jeu d'échecs
+                Agnes Obel (née en 1980), chanteuse
+                Sigbjørn Obstfelder (1866 - 1900), poète norvégien
+                Verner Panton (1926-1998), designer
+                Josephine Skriver (née en 1993), mannequin
+                Christian Thomas (1896-1970), gymnaste
+                Hans Peter Jörgen Julius Thomsen (1826-1909), chimiste
+                Mike Tramp (née en 1961), chanteur de White Lion
+                Lars Ulrich (né en 1963), batteur de Metallica
+                Serge Vandercam (1924-2005), artiste
+                Lars von Trier (né en 1956), réalisateur
+                Kristen Jensen Lyngby (1829-1871), philologue
+            </p>
+
+            <h3>Films tournés à Capenhague</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                1922/23 : Inga Larsen de Hans Steinhoff avec Henny Porten (scènes tournées en avril et mai 1923)
+                2001 : La Répétition de Catherine Corsini avec Emmanuelle Béart, Pascale Bussières (scènes tournées en 2001)
+                Films de Nicolas Winding Refn : Bleeder en 1999, la trilogie Pusher (avec Mads Mikkelsen) en 1996, 2004 et 2005
+                2007 : Room 205 (Kollegiet) de Martin Barnewitz
+                2015 : Pitch Perfect 2 de Elizabeth Banks (Les scènes avant les mondiaux sont tournées à Copenhague mais les mondiaux sont tournés à Bâton-Rouge)
+                2015 : Danish Girl de Tom Hooper
+            </p>
+            
+            <p>source :
+                <a href="https://fr.wikipedia.org/wiki/Copenhague" class="reference">Wikipédia - Ville de Capenhague</a>
+            </p>
+            
+        </section>
+    </main>
+
+    <footer>
+        <?php
+        // chemin absolu
+        include ROOT_PATH."/view/inc/footer.php";
+        // chemin relatif (en PHP à partir de la page qui appelle)
+        # include "inc/footer.php";
+
+        ?>
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
--- view/histoire.php
+++ view/histoire.php
@@ -34,24 +34,52 @@
                 Ces découvertes indiquent que les origines de Copenhague en tant que ville remontent au moins au xie siècle. De plus, les découvertes importantes d'outils en silex de la région fournissent des preuves des établissements humains datant de l'Âge de la pierre. Plusieurs historiens croient que la ville moderne de Copenhague prend racine à la fin de l'Âge des Vikings et que celle-ci a été probablement fondée par Sven Ier de Danemark.
                 Des fouilles ont mis en évidence l'existence de deux colonies au xie siècle. La première était située entre les actuelles rues Mikkel Bryggersgade, Vestergade, Gammeltorv/Nytorv et Løngangsstræde, ce qui correspondait à peu près à la ligne côtière de l'époque. La seconde, plus petite, correspond à l'actuel Kongens Nytorv.
             </p>
+            <h3>Moyen Age</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                La première mention de la ville se situe en 1043 dans la Knýtlinga saga sous le nom de port (Hafnæ, puis Hafnia), où il est dit que Sven II de Danemark s'y réfugie après avoir été battu par Magnus Ier de Norvège. Dans la Geste des Danois, écrite dans les années 1200, Saxo Grammaticus se réfère à la ville sous le nom de Portus Mercatorum, soit en danois de l'époque Købmannahavn, qui signifie le port des marchands.
 
-            <p>
-                <a href="../?p=geographie" class="liens-utiles">Ici nous parlerons de la vile de Capenhague, une présentation envers le Danemark et son role dans le monde</a>
+                En 1157, Valdemar Ier de Danemark fait don de la ville et des villages des environs à Absalon, évêque de Roskilde7. La lettre de don originale est perdue7 mais la lettre de confirmation du pape Urbain III du 21 octobre 1186 est conservée8. Absalon y construit alors un château en 1167, ce qui marqua le début de la montée en puissance de la ville. Durant les années qui suivirent, la taille de la ville décupla, plusieurs églises et abbayes furent construites (dont la cathédrale Notre-Dame, détruite aujourd'hui, l'édifice actuel datant de 1829 ; l'église du Saint-Esprit et l'église Saint-Pierre) et l'économie se développa grâce au commerce du hareng.
+
+                À la mort d'Absalon, en 1201, la ville et le château passa aux mains du diocèse de Roskilde. Le xiiie siècle a été une période mouvementée dans l'histoire de la ville, qui s'est exprimée dans la lutte acharnée des évêques et des rois changeants pour la domination sur la ville. Peu à peu, la ville a commencé à devenir la plus grande et la plus importante du royaume, même si elle n'était pas encore devenue la capitale. L'emplacement au milieu du royaume avec un port naturel par une importante route commerciale maritime était idéal. Même si la ville était la plus grande, il y avait encore moins de 5 000 habitants.
+
+                En 1419, le roi danois, Éric de Poméranie, réussit finalement à prendre définitivement le pouvoir sur la ville de l'église, et en 1443, le roi Christophe III fit de la ville une résidence royale. En 1479, l'université de Copenhague a été fondée. Copenhague était désormais la ville la plus importante du pays.
+
+                Vers 1530, la guerre entre catholiques et protestants tournent à l’avantage des seconds.
+                Copenhague profitera de l’indépendance des Pays-Bas (Provinces-Unis), de la croissance d’Amsterdam et de l’augmentation du commerce sur la mer Baltique.
+
             </p>
-            <p>
-                <a href="../?p=histoire" class="liens-utiles">Un rappel historique, de sa fondation à aujourd'hui</a>
+            <h3>L’âge d’or de Copenhague sous Christian IV</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Au moment du couronnement de Christian IV en 1596, Copenhague est riche et puissante.
+
+                Le nouveau roi a décidé de faire de la ville le centre économique, militaire, religieux et culturel pour l’ensemble de la région nordique.
+                Le roi a créé les premières entreprises commerciales ayant le droit exclusif de négocier avec des terres à l’étranger sur le principe de la Compagnie des Indes néerlandaises. Afin de limiter les importations, des usines sont créées.
+
+                Christian IV agrandit Copenhague en ajoutant deux nouveaux quartiers :
+                Nyboder pour accueillir les membres de la marine et leur famille,
+                Christianshavn inspiré d’Amsterdam.
+
+                Des architectes allemands et néerlandais construisent des édifices remarquables pour renforcer son prestige : Rosenborg, Rundetaarn (église de la Trinité), Tøjhus Arsenal et Børsen, la bourse de Copenhague.
+                Des fortifications modernes toujours visibles sous la végétation de Christiania sont construites.
+
             </p>
-            <p>
-                <a href="../?p=culture" class="liens-utiles">Un tour d'horizon en matiere d'art et culture</a>
+
+            <h3>Catastrophes, paix et guerre</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Une épidémie de peste (1711) et des incendies (1728 et 1795) réduisent la population et détruisent une grande partie de la Vieille Ville de Copenhague.
+                Dans ce contexte, le Danemark reste neutre dans les conflits qui secouent l’Europe de l’époque.
+                De cette neutralité prospère naîssent les Palais d’Amalienborg et la version visible aujourd’hui du Palais de Christiansborg.
+
+                En 1801 et 1807, les Anglais bombardent Copenhague pour forcer l’alliance des Danois contre Napoléon.
+                Des bombes à phosphore inéteignables avec de l’eau sont utilisées sur des cibles civiles. On compte de nombreux morts. Habitations et églises sont en ruine.
+
             </p>
-            <p>
-                <a href="../?p=galerie" class="liens-utiles">Une galerie de photographie</a>
-            </p>
-            <p>
-                <a href="../?p=contact" class="liens-utiles">Un formulaire pour nous contacter</a>
-            </p>
-            <p>
-                <a href="../?p=liens" class="liens-utiles">Des liens vers les sites partenaires</a>
+
+            <h3>L’age moderne de Copenhague</h3>
+            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                Vers 1850, les fortifications à l’ouest de Copenhague sont abattus et les terrains gagnés permettront de construire de nouveaux quartiers : Nørrebro et Vesterbro. 
+                Le port est agrandie vers 1894. Les tramways électriques font leur entrée vers 1897.
+                La population explose. Il y avait 120 000 habitants en 1840, il y en aura 400 000 en 1901.
             </p>
             
             <p>source :
