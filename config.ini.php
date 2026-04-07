d6ec9bd6766a366a4cd1e51e63cf69230aae8104
John-Dav9 <tchomguijohn@gmail.com>
2026-04-07 11:20:38 +0200

    configuration du fichier config.ini.php

---
+++ config.ini.php
@@ -0,0 +1,14 @@
+<!-- Configuration  -->
+
+<?php
+
+const ROOT_PATH = __dir__;
+
+const ARRAY_VALID_PAGES = [
+    'geographie',
+    'histoire',
+    'culture',
+    'galerie',
+    'contact',
+    'liens',
+];
