0afc27a4851ee1ceb694b60709af23e0bdad0c93
John-Dav9 <tchomguijohn@gmail.com>
2026-04-07 15:57:57 +0200

    Modification de la page culture (ajout des listes à puces à la place des paragraphues

--- view/culture.php
+++ view/culture.php
@@ -12,124 +12,127 @@
         <h1>CAPENHAGUE</h1>
     </div>
 
-    <header>
-        <?php
-        // chemin absolu
-        include ROOT_PATH."/view/inc/menu.php";
-        // chemin relatif (en PHP à partir de la page qui appelle)
-        # include "inc/menu.php";
+    <div>
+        <header>
+            <?php
+            // chemin absolu
+            include ROOT_PATH."/view/inc/menu.php";
+            // chemin relatif (en PHP à partir de la page qui appelle)
+            # include "inc/menu.php";
 
-        ?>
-    </header>
+            ?>
+        </header>
 
-    <main>
-        <section style="text-align: center; margin-top: 10vh;">
-            <h1>Histoire</span></h1>
-            <img src="../img/culture.png" alt="Culture de Capenhagen">
+        <main>
+            <section style="text-align: center; margin-top: 10vh;">
+                <h1>Histoire</span></h1>
+                <img src="../img/culture.png" alt="Culture de Capenhagen">
 
-            <h3>Bibliothèques</h3>
-            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
-                Le Diamant noir (1999) qui abrite la bibliothèque nationale du Danemark conçue par les architectes Schmidt Hammer Lassen.
-                Il existe à Copehague 20 bibliothèques dont une principale qui est la plus grande du pays. Celle-ci est située dans le quartier latin de la ville.
+                <h3>Bibliothèques</h3>
+                <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                    Le Diamant noir (1999) qui abrite la bibliothèque nationale du Danemark conçue par les architectes Schmidt Hammer Lassen.
+                    Il existe à Copehague 20 bibliothèques dont une principale qui est la plus grande du pays. Celle-ci est située dans le quartier latin de la ville.
 
-            </p>
-            <h3>Chateaux</h3>
-            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
-                Palais Amalienborg
-                Christiansborg
-                Château de Frederiksborg à Hillerød
-                Château Kronborg à Helsingør
-                Château de Rosenborg
-                Rundetårn
-                Kastellet
-            </p>
-            <h3>Musées</h3>
-            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
-                Statens Museum for Kunst (Musée des Beaux-arts danois)
-                Musée d'art moderne Louisiana à Humlebæk
-                Musée national du Danemark
-                Ny Carlsberg Glyptotek
-                La collection Hirschsprung
-                Musée de la liberté (Frihedsmuseet)
-                Museum Erotica (Musée de l'érotisme) (fermé)
-                Eksperimentarium (musée de physique expérimentale)
-                Planétarium Tycho Brahé
-            </p>
+                </p>
+                <h3>Chateaux</h3>
+                <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                    <li>Palais Amalienborg</li>
+                    <li>Château de Frederiksborg à Hillerød</li>Château de Frederiksborg à Hillerød
+                    <li>Château de Frederiksborg à Hillerød</li>
+                    <li>Château Kronborg à Helsingør</li>
+                    <li>Château de Rosenborg</li>
+                    <li>Rundetårn</li>
+                    <li>Kastellet</li>
+                </p>
+                <h3>Musées</h3>
+                <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                   <li>Statens Museum for Kunst (Musée des Beaux-arts danois)</li> 
+                   <li>Musée d'art moderne Louisiana à Humlebæk</li> 
+                   <li>Musée national du Danemark</li> 
+                   <li>Ny Carlsberg Glyptotek</li> 
+                   <li>La collection Hirschsprung</li> 
+                   <li>Museum Erotica (Musée de l'érotisme) (fermé)</li> 
+                   <li>Eksperimentarium (musée de physique expérimentale)</li> 
+                   <li>Planétarium Tycho Brahé</li> 
+                </p>
 
-            <h3>Jardins</h3>
-            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
-                Bakken : l'un des plus anciens parcs d'attractions du monde
-                Parc des daims: en dehors de Copenhague, au sein duquel se trouve Bakken
-                Copenhagen Zoo
-                Kongens Have : jardin royal
-                Jardins de Tivoli : parc d'attractions
-                Fælledparken : le plus grand parc urbain de Copenhague
-                Cimetière Assistens Kirkegaard : un cimetière datant de 1760
-            </p>
+                <h3>Jardins</h3>
+                <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                   <li>Bakken : l'un des plus anciens parcs d'attractions du monde</li> 
+                   <li>Parc des daims: en dehors de Copenhague, au sein duquel se trouve Bakken</li> 
+                   <li>Copenhagen Zoo</li> 
+                   <li>Kongens Have : jardin royal</li> 
+                   <li>Jardins de Tivoli : parc d'attractions</li> 
+                   <li>Fælledparken : le plus grand parc urbain de Copenhague</li> 
+                   <li>Cimetière Assistens Kirkegaard : un cimetière datant de 1760</li> 
+                </p>
 
-            <h3>Personnages connus, associés à Capenhague</h3>
-            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
-                Hans Christian Andersen (1805-1875), écrivain
-                Mianne Bagger (1966-), golfeuse
-                Anthon Bang (1809-1870), écrivain et éditeur norvégien
-                Niklas Björnberg (1758-1829), homme d'affaires et directeur de la Compagnie suédoise des Indes orientales
-                Niels Bohr (1885-1962), physicien
-                Jens Bodtker Rasmussen (1947-2005), herpétologiste
-                Tycho Brahe (1546-1601), astronome
-                Aura Dione (née en 1985), chanteuse.
-                Nanna Ditzel (1923-2005), architecte et designer
-                Helen Dohlmann (1870-1942), sculptrice
-                Carl Theodor Dreyer (1889-1968), cinéaste
-                Giancarlo Esposito (né en 1958), acteur
-                Emma Gad (1852-1921), écrivaine
-                Niels Wilhelm Gade (1817-1890), compositeur
-                Jan Gehl (né en 1936), urbaniste et architecte
-                Rolf Graae (1916-1996), architecte
-                Nikolai Grundtvig (1783-1872), philosophe, écrivain et poète
-                August Wilhelm Hartmann (1775-1850), compositeur danois
-                Johann Peter Emilius Hartmann (1805-1900), compositeur danois
-                Ludvig Holberg (1684-1754), historien et dramaturge
-                Bodil Ipsen (1889-1964), réalisatrice
-                Victor Isbrand (1897-1989), peintre
-                Arne Jacobsen (1902-1971), architecte
-                Robert Jacobsen (1912-1993), artiste
-                Grete Jalk (1920–2006), designer
-                Niels Peter Jensen (1802 -1846), compositeur, flûtiste et organiste
-                Søren Kierkegaard (1813-1855), philosophe
-                Per Kirkeby (né en 1938) artiste
-                Mads Mikkelsen (né en 1965), acteur
-                Georg Mohr (1640-1697), mathématicien
-                Carl Nielsen (1865-1931), compositeur
-                Aaron Nimzowitsch (1886-1935), théoricien du jeu d'échecs
-                Agnes Obel (née en 1980), chanteuse
-                Sigbjørn Obstfelder (1866 - 1900), poète norvégien
-                Verner Panton (1926-1998), designer
-                Josephine Skriver (née en 1993), mannequin
-                Christian Thomas (1896-1970), gymnaste
-                Hans Peter Jörgen Julius Thomsen (1826-1909), chimiste
-                Mike Tramp (née en 1961), chanteur de White Lion
-                Lars Ulrich (né en 1963), batteur de Metallica
-                Serge Vandercam (1924-2005), artiste
-                Lars von Trier (né en 1956), réalisateur
-                Kristen Jensen Lyngby (1829-1871), philologue
-            </p>
+                <h3>Personnages connus, associés à Capenhague</h3>
+                <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                    <li>Hans Christian Andersen (1805-1875), écrivain</li>
+                    <li>Mianne Bagger (1966-), golfeuse</li>
+                    <li>Anthon Bang (1809-1870), écrivain et éditeur norvégien</li>
+                    <li>Niklas Björnberg (1758-1829), homme d'affaires et directeur de la Compagnie suédoise des Indes orientales</li>
+                    <li>Niels Bohr (1885-1962), physicien</li>
+                    <li>Jens Bodtker Rasmussen (1947-2005), herpétologiste</li>
+                    <li>Tycho Brahe (1546-1601), astronome</li>
+                    <li>Aura Dione (née en 1985), chanteuse.</li>
+                    <li>Nanna Ditzel (1923-2005), architecte et designer</li>
+                    <li>Helen Dohlmann (1870-1942), sculptrice</li>
+                    <li>Carl Theodor Dreyer (1889-1968), cinéaste</li>
+                    <li>Giancarlo Esposito (né en 1958), acteur</li>
+                    <li>Emma Gad (1852-1921), écrivaine</li>
+                    <li>Niels Wilhelm Gade (1817-1890), compositeur</li>
+                    <li>Jan Gehl (né en 1936), urbaniste et architecte</li>
+                    <li>Rolf Graae (1916-1996), architecte</li>
+                    <li>Nikolai Grundtvig (1783-1872), philosophe, écrivain et poète</li>
+                    <li>August Wilhelm Hartmann (1775-1850), compositeur danois</li>
+                    <li>Johann Peter Emilius Hartmann (1805-1900), compositeur danois</li>
+                    <li>Ludvig Holberg (1684-1754), historien et dramaturge</li>
+                    <li>Bodil Ipsen (1889-1964), réalisatrice</li>
+                    <li>Victor Isbrand (1897-1989), peintre</li>
+                    <li>Arne Jacobsen (1902-1971), architecte</li>
+                    <li>Robert Jacobsen (1912-1993), artiste</li>
+                    <li>Grete Jalk (1920–2006), designer</li>
+                    <li>Niels Peter Jensen (1802 -1846), compositeur, flûtiste et organiste</li>
+                    <li>Søren Kierkegaard (1813-1855), philosophe</li>
+                    <li>Per Kirkeby (né en 1938) artiste</li>
+                    <li>Mads Mikkelsen (né en 1965), acteur</li>
+                    <li>Georg Mohr (1640-1697), mathématicien</li>
+                    <li>Carl Nielsen (1865-1931), compositeur</li>
+                    <li>Aaron Nimzowitsch (1886-1935), théoricien du jeu d'échecs</li>
+                    <li>Agnes Obel (née en 1980), chanteuse</li>
+                    <li>Sigbjørn Obstfelder (1866 - 1900), poète norvégien</li>
+                    <li>Verner Panton (1926-1998), designer</li>
+                    <li>Josephine Skriver (née en 1993), mannequin</li>
+                    <li>Christian Thomas (1896-1970), gymnaste</li>
+                    <li>Hans Peter Jörgen Julius Thomsen (1826-1909), chimiste</li>
+                    <li>Mike Tramp (née en 1961), chanteur de White Lion</li>
+                    <li>Lars Ulrich (né en 1963), batteur de Metallica</li>
+                    <li>Serge Vandercam (1924-2005), artiste</li>
+                    <li>Lars von Trier (né en 1956), réalisateur</li>
+                    <li>Kristen Jensen Lyngby (1829-1871), philologue</li>
+                </p>
 
-            <h3>Films tournés à Capenhague</h3>
-            <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
-                1922/23 : Inga Larsen de Hans Steinhoff avec Henny Porten (scènes tournées en avril et mai 1923)
-                2001 : La Répétition de Catherine Corsini avec Emmanuelle Béart, Pascale Bussières (scènes tournées en 2001)
-                Films de Nicolas Winding Refn : Bleeder en 1999, la trilogie Pusher (avec Mads Mikkelsen) en 1996, 2004 et 2005
-                2007 : Room 205 (Kollegiet) de Martin Barnewitz
-                2015 : Pitch Perfect 2 de Elizabeth Banks (Les scènes avant les mondiaux sont tournées à Copenhague mais les mondiaux sont tournés à Bâton-Rouge)
-                2015 : Danish Girl de Tom Hooper
-            </p>
-            
-            <p>source :
-                <a href="https://fr.wikipedia.org/wiki/Copenhague" class="reference">Wikipédia - Ville de Capenhague</a>
-            </p>
-            
-        </section>
-    </main>
+                <h3>Films tournés à Capenhague</h3>
+                <p style="font-size: 1.2rem; max-width: 600px; margin: 0 auto 2rem; color: var(--text-light);">
+                    <ul>
+                        <li>1922/23 : Inga Larsen de Hans Steinhoff avec Henny Porten (scènes tournées en avril et mai 1923)</li>
+                        <li>2001 : La Répétition de Catherine Corsini avec Emmanuelle Béart, Pascale Bussières (scènes tournées en 2001)</li>
+                        <li>Films de Nicolas Winding Refn : Bleeder en 1999, la trilogie Pusher (avec Mads Mikkelsen) en 1996, 2004 et 2005</li>
+                        <li>2007 : Room 205 (Kollegiet) de Martin Barnewitz</li>
+                        <li>2015 : Pitch Perfect 2 de Elizabeth Banks (Les scènes avant les mondiaux sont tournées à Copenhague mais les mondiaux sont tournés à Bâton-Rouge)</li>
+                        <li>2015 : Danish Girl de Tom Hooper</li>
+                    </ul>
+                </p>
+                
+                <p>source :
+                    <a href="https://fr.wikipedia.org/wiki/Copenhague" class="reference">Wikipédia - Ville de Capenhague</a>
+                </p>
+                
+            </section>
+        </main>
+    </div>
 
     <footer>
         <?php
